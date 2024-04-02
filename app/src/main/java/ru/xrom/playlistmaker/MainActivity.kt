ddf328ff2311b48b1b2bc760aadcba67ec8d4e93
Roman <mih-xrom@ya.ru>
2024-04-02 18:33:06 +0300

    Toast task completed

--- app/src/main/java/ru/xrom/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/xrom/playlistmaker/MainActivity.kt
@@ -1,16 +1,30 @@
 package ru.xrom.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val btnSearch = findViewById<Button>(R.id.btn_search)
+        btnSearch.setOnClickListener {
+            Toast.makeText(this@MainActivity,"Вы нажали: "+getString(R.string.searchBtn_text),Toast.LENGTH_SHORT).show()
+        }
+        val btnLibrary = findViewById<Button>(R.id.btn_library)
+        val btnLibraryClickListener: View.OnClickListener = object : View.OnClickListener { override fun onClick(v: View?) {
+            Toast.makeText(this@MainActivity,"Вы нажали: "+getString(R.string.libraryBtn_text),Toast.LENGTH_SHORT).show()
+        } }
+        btnLibrary.setOnClickListener(btnLibraryClickListener)
+
+        val btnSettings = findViewById<Button>(R.id.btn_settings)
+        btnSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity,"Вы нажали: "+getString(R.string.searchBtn_text),Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
