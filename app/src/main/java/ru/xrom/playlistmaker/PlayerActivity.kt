2d4f3a33acb682587cd204843e701c61d47c01a1
Roman <mih-xrom@ya.ru>
2024-06-15 21:52:37 +0300

    Fix from dev branch

--- app/src/main/java/ru/xrom/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/ru/xrom/playlistmaker/PlayerActivity.kt
@@ -9,7 +9,7 @@ import androidx.appcompat.widget.Toolbar
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import ru.xrom.playlistmaker.utils.Track
-import ru.xrom.playlistmaker.utils.Utils.dpToPx
+import ru.xrom.playlistmaker.utils.dpToPx
 import java.util.Locale
 
 class PlayerActivity : AppCompatActivity() {
--- app/src/main/java/ru/xrom/playlistmaker/recycleView/TrackViewHolder.kt
+++ app/src/main/java/ru/xrom/playlistmaker/recycleView/TrackViewHolder.kt
@@ -9,7 +9,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import ru.xrom.playlistmaker.R
 import ru.xrom.playlistmaker.utils.Track
-import ru.xrom.playlistmaker.utils.Utils.dpToPx
+import ru.xrom.playlistmaker.utils.dpToPx
 import java.util.Locale
 
 
