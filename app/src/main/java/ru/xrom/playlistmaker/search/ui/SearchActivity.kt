e8e5089c5572d24bff18e59ba0cf3e3ef8d09b13
Roman <mih-xrom@ya.ru>
2024-08-13 20:02:39 +0300

    cleanup code

--- app/src/main/java/ru/xrom/playlistmaker/search/ui/SearchActivity.kt
+++ app/src/main/java/ru/xrom/playlistmaker/search/ui/SearchActivity.kt
@@ -117,7 +117,6 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
-
     private fun renderState(state: SearchState) {
         when (state) {
             is SearchState.ContentHistory -> updateSearchHistoryAdapter(state)
