8b0e05ff5c016ec71fd77f0de769b12c31f6f605
Roman <mih-xrom@ya.ru>
2024-10-31 11:17:29 +0300

    Add playlist layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,9 +63,8 @@ dependencies {
     implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
 
-    implementation(libs.androidx.room.runtime) // библиотека Room
+    implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
-    implementation(libs.androidx.room.ktx)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
